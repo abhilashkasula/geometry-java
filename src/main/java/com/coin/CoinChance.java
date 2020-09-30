19c673011a2433389d1b61fe362be0a4125baa2d
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-09-30 10:52:36 +0530

    renamed CoinSide to CoinChance

--- src/main/java/com/coin/CoinChance.java
+++ src/main/java/com/coin/CoinChance.java
@@ -1,17 +1,15 @@
 package com.coin;
 
 public class CoinChance {
-
   private final int noOfCoins;
 
   public CoinChance(int noOfCoins) {
-
     this.noOfCoins = noOfCoins;
   }
 
   public double gettingTails() {
-    final double sides = Math.pow(2, this.noOfCoins);
-    return 1 / sides;
+    final double possibleOutcomes = Math.pow(2, this.noOfCoins);
+    return 1 / possibleOutcomes;
   }
 
   public double notGettingTails() {

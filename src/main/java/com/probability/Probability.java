be92c65dfd91dd0e33c1b09953eb7b765733b219
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-10-01 11:31:34 +0530

    added and method to Probability

--- src/main/java/com/probability/Probability.java
+++ src/main/java/com/probability/Probability.java
@@ -3,31 +3,35 @@ package com.probability;
 import java.util.Objects;
 
 public class Probability {
-  public static final int MAX_PROBABILITY = 1;
-  private final double value;
+    public static final int MAX_PROBABILITY = 1;
+    private final double value;
 
-  public Probability(double value) {
-    this.value = value;
-  }
+    public Probability(double value) {
+        this.value = value;
+    }
 
-  public Probability complement() {
-    return new Probability(MAX_PROBABILITY - this.value);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 
-  public Probability combine(Probability probability) {
-    return new Probability(this.value * probability.value);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Probability that = (Probability) o;
+        return Double.compare(that.value, value) == 0;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Probability that = (Probability) o;
-    return Double.compare(that.value, value) == 0;
-  }
+    public Probability or(Probability other) {
+        return this.complement().and(other.complement()).complement();
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(value);
-  }
+    public Probability complement() {
+        return new Probability(MAX_PROBABILITY - this.value);
+    }
+
+    public Probability and(Probability other) {
+        return new Probability(this.value * other.value);
+    }
 }
--- src/test/java/com/probability/ProbabilityTest.java
+++ src/test/java/com/probability/ProbabilityTest.java
@@ -1,21 +1,30 @@
 package com.probability;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProbabilityTest {
     @Test
     void shouldComplementTheProbability() {
-        final Probability probability = new Probability(0.5);
-        final Probability expectedProbability = new Probability(0.5);
+        final Probability probability = new Probability(0.49);
+        final Probability expectedProbability = new Probability(0.51);
         assertEquals(expectedProbability, probability.complement());
     }
 
     @Test
-    void shouldCombineTwoProbabilities() {
-        final Probability probability1 = new Probability(0.5);
-        final Probability probability2 = new Probability(0.5);
-        final Probability expectedProbability = new Probability(0.25);
-        assertEquals(expectedProbability, probability1.combine(probability2));
+    void shouldPerformAndOnTwoProbabilities() {
+        final Probability probability1 = new Probability(0.49);
+        final Probability probability2 = new Probability(0.51);
+        final Probability expectedProbability = new Probability(0.2499);
+        assertEquals(expectedProbability, probability1.and(probability2));
+    }
+
+    @Test
+    void shouldPerformOrOnTwoProbabilities() {
+        final Probability probability1 = new Probability(0.49);
+        final Probability probability2 = new Probability(0.51);
+        final Probability probability = new Probability(0.7501);
+        assertEquals(probability, probability1.or(probability2));
     }
 }
